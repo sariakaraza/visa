3a0ee9489faefe4d349358a9661b552513aa6323
Sandratra Rabenanahary <sandratrahasinarivelo5@gmail.com>
2026-04-16 15:02:40 +0300

    initialisation du projet

---
+++ visa/src/main/java/com/visa/VisaApplication.java
@@ -0,0 +1,13 @@
+package com.visa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VisaApplication.class, args);
+    }
+
+}
---
+++ visa/src/test/java/com/visa/VisaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.visa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
