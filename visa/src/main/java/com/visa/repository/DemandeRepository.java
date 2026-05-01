6f7bc043488be08d515445d8887f6536c394f73e
laingotiana <ratovoarinirinalaingotiana@gmail.com>
2026-05-01 22:07:47 +0300

    Ajout fonction pour la generation du qr code

--- visa/src/main/java/com/visa/repository/DemandeRepository.java
+++ visa/src/main/java/com/visa/repository/DemandeRepository.java
@@ -4,6 +4,9 @@ import com.visa.entity.Demande;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface DemandeRepository extends JpaRepository<Demande, Integer> {
+	Optional<Demande> findByReferenceDemandeIgnoreCase(String referenceDemande);
 }
